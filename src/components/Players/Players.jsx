d5af7fdab2ec95488f5d5d085350af9e3f0d61cd
Thomas Shanley <102588210@student.swin.edu.au>
2022-11-13 18:30:40 +1100

    profiles

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Players.css'
-// import profile1 from '../../assets/rj.png'
+import profile1 from '../../assets/adin3.jpg'
+import profile2 from '../../assets/logo.png'
+import profile3 from '../../assets/logo.png'
 import { FaUserAlt } from 'react-icons/fa'
 
 
@@ -10,22 +12,40 @@ const Players = () => {
       <h1 id='players-title'>PLAYERS</h1>
       <div className='player-profiles'>
       <div id="profile1">
-        {/* <img className='profile-image' src={profile1}></img> */}
-        <FaUserAlt size={200} className='profile-image'/>
-        <h3 className='profile-name'>Players Name</h3>
-        <p className='profile-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum esse doloribus beatae porro aperiam at aliquam soluta repellat incidunt. Animi alias ducimus ut voluptate nisi repellendus mollitia ex aliquam.
+        <div className="image-cropper">
+        <img id='profile-pic1'src={profile1} alt={<FaUserAlt size={200} className='profile-image'/>} />
+        </div>
+        <h3 className='profile-name'>Adin Baxter</h3>
+        <p className='profile-text'>
+          <strong>IPR Rank: </strong>1.02<br/> 
+          <strong>Dominant Hand:</strong> Right <br />
+          <strong>Strengths:</strong> Facebook friends with Ben Ratcliffe (WR271)<br/>
+          Champion Player of Wednesday 2022 Spring Internal Comp Division 7. <br />
+          <strong>Weaknesses: </strong>Recepetionists
         </p>
       </div>
       <div id="profile2">
-        <FaUserAlt size={200} className='profile-image'/>
-        <h3 className='profile-name'>Players Name</h3>
-        <p className='profile-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum esse doloribus beatae porro aperiam at aliquam soluta repellat incidunt. Animi alias ducimus ut voluptate nisi repellendus mollitia ex aliquam.
+        <div className="image-cropper">
+          <img id='profile-pic2'src={profile2} alt={<FaUserAlt size={200} className='profile-image'/>} />
+        </div>
+        <h3 className='profile-name'>Thomas Shanley</h3>
+        <p className='profile-text'>
+        <strong>IPR Rank: </strong>0.34<br/> 
+        <strong>Dominant Hand:</strong> Left<br />
+        <strong>Strengths: </strong> <br />
+        <strong>Weaknesses: </strong>Law enforcement
         </p>
       </div>
       <div id="profile3">
-        <FaUserAlt size={200} className='profile-image'/>
-        <h3 className='profile-name'>Players Name</h3>
-        <p className='profile-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum esse doloribus beatae porro aperiam at aliquam soluta repellat incidunt. Animi alias ducimus ut voluptate nisi repellendus mollitia ex aliquam.
+        <div className="image-cropper">
+         <img id='profile-pic3'src={profile3} alt={<FaUserAlt size={200} className='profile-image'/>} />
+        </div>
+        <h3 className='profile-name'>Ronan Johnson</h3>
+        <p className='profile-text'>
+        <strong>IPR Rank: </strong>0.31<br/> 
+        <strong>Dominant Hand:</strong> Right <br />
+        <strong>Strengths: </strong>Determination, nice car, Calton Draught trivia questions <br />
+        <strong>Weaknesses: </strong>Large Zinger boxes
         </p>
       </div>
       </div>
