3e64791faf6af8517bcde77038064062c2f03558
tomsshanley <81658880+tomsshanley@users.noreply.github.com>
2023-09-03 17:03:48 +0930

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,7 @@ const App = () => {
   return (
     <div>
       
-    <h1>Our Response</h1>
-      <p>Subject: Re: Accusations of Sexting Scandal - Strong Denial and Counterattack
-
-I must address the outrageous allegations circulating about our squash organization's manager. Let me be crystal clear: these accusations are utterly false and nothing more than a malicious smear campaign orchestrated by DMG, our rival squash management company.
-
-Our manager, who has consistently displayed professionalism and integrity throughout their tenure, is the target of baseless slander. It is disheartening to witness DMG resorting to such deceitful tactics in a desperate attempt to tarnish our reputation.
-
-DMG's reckless spread of fake news is a blatant attempt to undermine our credibility and manipulate public perception. We challenge DMG to present any credible evidence to substantiate their claims, knowing full well that they won't be able to provide any.
-
-We have no choice but to view DMG's actions as a deliberate and malicious attempt to damage our organization's standing. We will not stand idly by while our reputation is dragged through the mud. We are prepared to take all necessary legal actions to defend our manager's honor and hold DMG accountable for their defamatory statements.
-
-In the interest of fairness and justice, we demand an immediate retraction of these false allegations from DMG, along with a public apology for the harm caused to our organization. We are more than willing to engage in any legal proceedings to ensure that the truth prevails.
-
-Sincerely,
-
-Thomas Shanley
-FIAB Management</p>
+    
         <Nav />
         <Showcase/>
         <Players/>
